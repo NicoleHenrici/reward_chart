8a767852c77a1740ac500f912272d2fb78b40dc6
Nicole <nicole@MacBookPro.fritz.box>
2025-09-07 14:56:44 +0200

    feature: index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import Modal from "@/components/modal";
+import NewTask from "@/components/newTask";
+
 
 export default function Home() {
+  const [click, setClick] = useState(false);
+
+  function handleNewRow() {
+    // Logic to add a new row to the table
+    console.log("New row button clicked");
+
+    setClick((prevClick) => !prevClick);
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <table className={`table ${styles.table}`}>
+          <thead>
+            <tr>
+              <th scope="col">AUFGABE</th>
+              <th scope="col">MONTAG</th>
+              <th scope="col">DIENSTAG</th>
+              <th scope="col">MITTWOCH</th>
+              <th scope="col">DONNERSTAG</th>
+              <th scope="col">FREITAG</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="button" className="btn btn-outline-primary" onClick={handleNewRow}>Neue Zeile</button>
+        {click && <Modal> 
+          <NewTask /> 
+        </Modal>}
       </main>
       <footer className={styles.footer}>
         <a
@@ -93,3 +91,4 @@ export default function Home() {
     </div>
   );
 }
+
