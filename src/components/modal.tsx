19f608a9d4136e6141139eac95dc086ddd7d383d
Nicole <nicole@MacBookPro.fritz.box>
2025-09-08 09:50:51 +0200

    refactor: lift state handling to parent component

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,24 +1,15 @@
 import styles from "./modal.module.css";
-import { useState } from "react";
+import { ModalProps } from "@/types/commonTypes";
 
-import { ReactNode } from "react";
+export default function Modal({ children, customFn }: ModalProps) {
 
-export default function Modal({ children }: { children: ReactNode }){
-const [isVisible, setIsVisible] = useState(true);
 
-  function closeHandler() {
-    setIsVisible(prevState => !prevState);
-  }
   return (
     <>
-    {isVisible && (
-      <>
-        <div className={styles.backdrop} onClick={closeHandler} />
-        <dialog open className={styles.modal}>
-          {children}
-        </dialog>
-      </>
-    )}
+      <div className={styles.backdrop} onClick={customFn} />
+      <dialog open className={`m-auto ${styles.modal}`}>
+        {children}
+      </dialog>
     </>
   );
 }
