60dede6e81007d5540b59f04bb49a21edb8847d2
Nicole <nicole@MacBookPro.fritz.box>
2025-09-09 09:42:05 +0200

    feature: replace placeholder with input and toggleHandler

--- src/components/table/tableRow.tsx
+++ src/components/table/tableRow.tsx
@@ -1,21 +1,25 @@
-"use client";
-
 import { TaskRecord } from "@/types/commonTypes";
-import { GoSmiley } from "react-icons/go";
 import { MdDelete } from "react-icons/md";
 
 type TableRowProps = {
-  task: TaskRecord;
-  removeTask: (id: number) => void;
+    task: TaskRecord;
+    removeTask: (id: number) => void;
+    toggleAccomplished?: (id: number, dayId: number) => void;
 };
 
-export default function TableRow({task, removeTask}: TableRowProps) {
+export default function TableRow({ task, removeTask, toggleAccomplished }: TableRowProps) {
 
     return (
         <tr key={task.id}>
             <th scope="row">{task.task}</th>
             {task.week.map((day, index) => (
-                <td key={index}>{day.accomplished ? <GoSmiley /> : "x"}</td>
+                <td key={index}><div className="form-check">
+                    <input className="form-check-input ms-auto float-none" type="checkbox" value="" id={`checkDefault-${index}`} 
+                    checked={day.accomplished} onChange={() => toggleAccomplished?.(task.id, day.id)} />
+                    <label className="form-check-label" htmlFor={`checkDefault-${index}`} hidden>
+                        Erledigt
+                    </label>
+                </div></td>
             ))}
             <td><button type="button" className="btn btn-link" onClick={() => removeTask(task.id)}><MdDelete /></button></td>
         </tr>
