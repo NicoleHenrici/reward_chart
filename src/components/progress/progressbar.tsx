27bf2675f184bc09be8bd206c53bca16fdd1e961
Nicole <nicole@MacBookPro.fritz.box>
2025-09-10 10:28:30 +0200

    fix: add % sign to progressbar

--- src/components/progress/progressbar.tsx
+++ src/components/progress/progressbar.tsx
@@ -10,7 +10,7 @@ export default function Progressbar({ valueNow, valueMin, valueMax, styleWidth }
     return (
         <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow={valueNow} aria-valuemin={valueMin ? valueMin : 0} 
         aria-valuemax={valueMax ? valueMax : 100}>
-            <div className="progress-bar text-bg-success" style={{ width: `${styleWidth}%` }}>{styleWidth}</div>
+            <div className="progress-bar text-bg-success" style={{ width: `${styleWidth}%` }}>{styleWidth}%</div>
         </div>
     );
 }
