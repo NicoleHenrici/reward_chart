ee5a5eed87b5c7a945c491a39cbf622c1b1c38a6
Nicole <nicole@MacBookPro.fritz.box>
2025-09-11 11:27:56 +0200

    feature(db): add CRUD methods

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -2,18 +2,18 @@ import Database from "better-sqlite3";
 
 const db = new Database("rewards_chart.db");
 
-function initDb(){
-    db.exec(`
+function initDb() {
+  db.exec(`
         CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             task_description TEXT NOT NULL
     )`);
-    db.exec(`
+  db.exec(`
         CREATE TABLE IF NOT EXISTS week (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             week_start_date REAL NOT NULL
     )`);
-    db.exec(`
+  db.exec(`
         CREATE TABLE IF NOT EXISTS task_completion (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             task_id INTEGER NOT NULL,
@@ -25,3 +25,30 @@ function initDb(){
 }
 
 initDb();
+
+export function addTask(taskDescription: string) {
+  const stmt = db.prepare(`
+        INSERT INTO tasks (task_description)
+        VALUES (?);
+    `);
+  stmt.run(taskDescription);
+}
+
+export function getTaskById(taskId: number) {
+  const stmt = db.prepare(`
+        SELECT *
+        FROM tasks
+        WHERE id = ?
+    `);
+
+  return stmt.get(taskId);
+}
+
+export function getAllTasks() {
+  const stmt = db.prepare(`
+        SELECT *
+        FROM tasks
+    `);
+
+  return stmt.all();
+}
