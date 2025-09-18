d3deb76c7c511ac288f83a8aaa2fafc4acb3507f
Nicole <nicole@MacBookPro.fritz.box>
2025-09-18 11:23:51 +0200

    refactor(taskLib): remove unused import

--- src/lib/tasks.ts
+++ src/lib/tasks.ts
@@ -1,5 +1,5 @@
 import { TaskRecord } from "@/types/commonTypes";
-import Database, { Statement } from "better-sqlite3";
+import Database from "better-sqlite3";
 
 const db = new Database("rewards_chart.db");
 
